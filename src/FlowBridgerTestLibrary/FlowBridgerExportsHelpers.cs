f39116e49e82f08047fb7e2ea7ba6b443b686646
Roman Vladimirov <trueromanus@gmail.com>
2025-09-10 14:34:36 +0300

    + Auto copy dll to output dir

--- src/FlowBridgerTestLibrary/FlowBridgerExportsHelpers.cs
+++ src/FlowBridgerTestLibrary/FlowBridgerExportsHelpers.cs
@@ -4,6 +4,7 @@
 
         public static partial double DigitalMethodInternal ( int int32Value, long int64Value, float floatValue, double doubleValue, uint uint32Value, ulong uint64Value ) {
             var isCorrect = int32Value == 1 && int64Value == 1000 && floatValue == 0.5f && doubleValue == 10.89 && uint32Value == 256 && uint64Value == 5559;
+            Console.WriteLine ( $"Passed is {(isCorrect ? "correct" : "incorrect")} parameters" );
 
             return isCorrect ? 138.890 : 0;
         }
