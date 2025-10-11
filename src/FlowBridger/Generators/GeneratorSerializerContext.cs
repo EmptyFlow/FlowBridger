cb03e288ca4fe4073201f6fa32cf1723898ff14d
Roman Vladimirov <trueromanus@gmail.com>
2025-10-11 19:30:18 +0300

    + Added omitted file

---
+++ src/FlowBridger/Generators/GeneratorSerializerContext.cs
@@ -0,0 +1,10 @@
+﻿using FlowBridger.Models;
+using System.Text.Json.Serialization;
+
+namespace FlowBridger.Generators {
+
+    [JsonSerializable ( typeof ( SchemaModel ) )]
+    internal partial class GeneratorSerializerContext : JsonSerializerContext {
+    }
+
+}
