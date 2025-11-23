b8b1e17a49f4bc71000c72004da6da8a19122bab
Roman Vladimirov <trueromanus@gmail.com>
2025-11-23 20:12:50 +0300

    + Added omitted file

---
+++ src/FlowBridger/Models/EventModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowBridger.Models {
+
+    internal record EventModel : MethodModel {
+
+        public bool InEvent { get; set; }
+
+        public bool OutEvent { get; set; }
+
+    }
+
+}
