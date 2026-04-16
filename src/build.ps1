0efa3450c7b123c8bb01cee1575a52eb3132e36f
Roman Vladimirov <trueromanus@gmail.com>
2026-04-16 14:55:54 +0300

    Added build for console application

---
+++ src/build.ps1
@@ -0,0 +1,2 @@
+dotnet publish -r win-x64 -c Release --self-contained true FlowBridger/FlowBridger.csproj
+Copy-Item -Path "FlowBridger\bin\Release\net10.0\win-x64\native\FlowBridger.exe" -Destination "C:\work\Repositories\flowbridger.exe" 
